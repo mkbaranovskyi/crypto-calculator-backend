bb7e7e6f2b86e62b22e40c2dba8289d163282edb
kravich13 <corlack1997@gmail.com>
2021-12-20 21:35:55 +0200

    fix hook

--- src/endpoints/auth/auth.router.ts
+++ src/endpoints/auth/auth.router.ts
@@ -6,9 +6,12 @@ import { signUpRouter, validateEmailRouter } from './auth.controller';
 const { secret } = jwtConfig;
 
 export const authRouter: FastifyPluginAsync<FastifyPluginOptions> = async (server, options) => {
-  server.addHook('preHandler', async (req, reply) => {
-    await checkAccessToken(secret, req.headers.authorization);
-  });
   await server.register(signUpRouter);
-  await server.register(validateEmailRouter);
+
+  await server.register(async (server, options) => {
+    server.addHook('preHandler', async (req, reply) => {
+      await checkAccessToken(secret, req.headers.authorization);
+    });
+    await server.register(validateEmailRouter);
+  });
 };
