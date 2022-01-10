2818d75eead9df836c93c8fbd12269855562ae36
Maksym Baranovskyi <mkbaranovskyi@gmail.com>
2022-01-10 19:06:03 +0200

    Docker initial setup added

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,11 @@
-import dotenv from 'dotenv';
-dotenv.config();
 import fastify from 'fastify';
 import { endpointRouter } from './endpoints/endpoint.router';
 import { connectToDB } from './shared/database';
 import { registerFastifySwagger } from './shared/plugins/swagger';
 import { LocalStorage } from './shared/services';
 
-const PORT = process.env.PORT ?? 5000;
+
+const PORT = Number(process.env.PORT) ?? 5000;
 const server = fastify({ logger: true });
 
 const start = async () => {
@@ -14,13 +13,7 @@ const start = async () => {
   registerFastifySwagger(server);
   await server.register(endpointRouter);
   await connectToDB();
-  server.listen(PORT, (err) => {
-    if (err) {
-      server.log.error(err);
-      process.exit(1);
-    }
-    console.log(`The server started on the PORT ${PORT}!`);
-  });
+  await server.listen({ port: PORT, host: '0.0.0.0' });
 };
 
 start();
