35dcd7d0530c0d8471735fc2fb7d02132c3681cd
Vladislav Onatskyi <vladislav.onatskiy@freshcodeit.net>
2023-03-13 19:40:04 +0200

    html text has been improved

--- src/endpoints/auth/index.ts
+++ src/endpoints/auth/index.ts
@@ -1,10 +1,7 @@
 import { FastifyPluginAsync, FastifyPluginOptions } from 'fastify';
-import { jwtConfig } from '../../shared/configs';
 import { refreshTokensController } from './refresh-tokens.controller';
 import { signInController } from './sign-in.controller';
 import { validateEmailController } from './validate-email.controller';
-
-const { secret } = jwtConfig;
 
 export const authRouter: FastifyPluginAsync<FastifyPluginOptions> = async (server, options) => {
   server.route(refreshTokensController).route(signInController).route(validateEmailController);
